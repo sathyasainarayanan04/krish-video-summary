c25338b922816c422aa543d8f0cf7987e16acfa7
sathyasainarayanan04 <sathyaofficial0404@gmail.com>
2024-10-20 15:38:03 +0530

    Create extract_frames.py

---
+++ extract_frames.py
@@ -0,0 +1,36 @@
+import cv2 
+import os
+
+def extract_frames(video_path, output_folder, frame_rate=1):
+    video = cv2.VideoCapture(video_path)
+
+    if not video.isOpened():
+        print("Error: Could not open video.")
+        return
+
+    fps = video.get(cv2.CAP_PROP_FPS)
+    interval = int(fps / frame_rate)
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    current_frame = 0
+    extracted_frame = 0
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        if current_frame % interval == 0:
+            frame_path = os.path.join(output_folder, f"frame_{extracted_frame:04d}.jpg")
+            cv2.imwrite(frame_path, frame)
+            extracted_frame += 1
+
+        current_frame += 1
+
+    video.release()
+    print(f"Extracted {extracted_frame} frames.")
+
+# Usage
+video_path = '/content/drive/MyDrive/hemorrhoids/5b87e720-5dfa-42b8-95bc-e40202be7404.avi'
+output_folder = '/content/drive/MyDrive/hemorrhoids2'
+extract_frames(video_path, output_folder)
